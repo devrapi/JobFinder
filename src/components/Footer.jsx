01f6b34943826cbc2bed7342f914987a1879e48b
unknown <raffymaranan22@gmail.com>
2025-02-22 13:23:58 +0800

    Last Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ const Footer = () => {
                 alt="Flowbite Logo"
               />
               <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+                Job Finder
               </span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            {/* <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
               <li>
                 <a href="#" class="hover:underline me-4 md:me-6">
                   About
@@ -40,13 +40,13 @@ const Footer = () => {
                   Contact
                 </a>
               </li>
-            </ul>
+            </ul> */}
           </div>
           <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
+            © 2025{" "}
             <a href="https://flowbite.com/" class="hover:underline">
-              Flowbite™
+              JobFinder
             </a>
             . All Rights Reserved.
           </span>
--- src/components/SearchQuery.jsx
+++ src/components/SearchQuery.jsx
@@ -14,7 +14,7 @@ const SearchQuery = ({ handleData, jobs }) => {
 
   return (
     <div className="p-4 sm:p-6 md:p-8 lg:p-10">
-      <form class="max-w-md mx-auto mt-10">
+      <div class="max-w-md mx-auto mt-10">
         <label
           for="default-search"
           class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -47,11 +47,14 @@ const SearchQuery = ({ handleData, jobs }) => {
             required
             onChange={(e) => setSearchVal(e.target.value)}
           />
-          {/* <button class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <button
+            onClick={(e) => setSearchVal(e.target.value)}
+            class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
             Search
-          </button> */}
+          </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
